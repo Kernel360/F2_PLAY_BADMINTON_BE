3fd1214d9ffd070e52813b5f82f66e44038063f1
Lee kangmin <km2535@naver.com>
2024-09-10 14:34:58 +0900

    [feat] init files

--- src/main/java/com/final2/odiya/OdiyaApplication.java
+++ src/main/java/com/final2/odiya/OdiyaApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.final2.odiya;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/final2/odiya/OdiyaApplicationTests.java
+++ src/test/java/com/final2/odiya/OdiyaApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.final2.odiya;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
