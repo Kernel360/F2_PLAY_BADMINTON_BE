c160f5dc01898669fb29b7bbd1071594d108811d
soeunnPark <carol0724@naver.com>
2024-09-13 18:24:47 +0900

    docs: multi module 초기 세팅

--- badminton-api/src/main/java/org/badminton/api/BadmintonApplication.java
+++ badminton-api/src/main/java/org/badminton/api/BadmintonApplication.java
@@ -1,4 +1,4 @@
-package com.final2.badminton;
+package org.badminton.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,7 +8,6 @@ public class BadmintonApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BadmintonApplication.class, args);
-
 	}
 
 }
