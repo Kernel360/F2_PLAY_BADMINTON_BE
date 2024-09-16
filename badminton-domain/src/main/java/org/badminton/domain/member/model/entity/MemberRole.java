b2d14d553b01d1d1231ae224fb093b6d456e0d53
I-migi <sunnn10189@gmail.com>
2024-09-17 03:24:12 +0900

    feat: MemberRole Getter 추가

--- badminton-domain/src/main/java/org/badminton/domain/member/model/entity/MemberRole.java
+++ badminton-domain/src/main/java/org/badminton/domain/member/model/entity/MemberRole.java
@@ -1,16 +1,16 @@
 package org.badminton.domain.member.model.entity;
 
+import lombok.Getter;
+
+@Getter
 public enum MemberRole {
 	ROLE_ADMIN("admin"),
 	ROLE_USER("user");
 
-	private String description;
+	private final String description;
 
 	MemberRole(String description) {
 		this.description = description;
 	}
 
-	public String getDescription() {
-		return description;
-	}
 }
