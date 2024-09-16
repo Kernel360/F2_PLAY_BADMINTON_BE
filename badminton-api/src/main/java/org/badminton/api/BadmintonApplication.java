083d1a40db14b914130f724d947eadb2890abfad
I-migi <sunnn10189@gmail.com>
2024-09-17 03:34:05 +0900

    feat: @EnableJpaAuditing 제거

--- badminton-api/src/main/java/org/badminton/api/BadmintonApplication.java
+++ badminton-api/src/main/java/org/badminton/api/BadmintonApplication.java
@@ -3,11 +3,9 @@ package org.badminton.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"org.badminton.api", "org.badminton.domain"})
-@EnableJpaAuditing
 public class BadmintonApplication {
 
 	public static void main(String[] args) {
