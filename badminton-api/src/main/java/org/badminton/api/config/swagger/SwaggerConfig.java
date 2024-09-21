17de4f783620e4b2898f018173948b88ccec5377
I-migi <sunnn10189@gmail.com>
2024-09-21 11:51:08 +0900

    feat: Swagger에 RequestHeader 추가

--- badminton-api/src/main/java/org/badminton/api/config/swagger/SwaggerConfig.java
+++ badminton-api/src/main/java/org/badminton/api/config/swagger/SwaggerConfig.java
@@ -9,14 +9,26 @@ import io.swagger.v3.core.jackson.ModelResolver;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 
 @Configuration
 public class SwaggerConfig {
 
 	@Bean
 	public OpenAPI openAPI() {
+		SecurityScheme securityScheme = new SecurityScheme()
+			.type(SecurityScheme.Type.HTTP)
+			.scheme("bearer")
+			.bearerFormat("JWT")
+			.in(SecurityScheme.In.HEADER)
+			.name("Authorization");
+
+		SecurityRequirement securityRequirement = new SecurityRequirement().addList("Authorization");
+
 		return new OpenAPI()
-			.components(new Components())
+			.components(new Components().addSecuritySchemes("Authorization", securityScheme))
+			.addSecurityItem(securityRequirement)
 			.info(apiInfo());
 	}
 
@@ -30,7 +42,5 @@ public class SwaggerConfig {
 	@Bean
 	public ModelResolver modelResolver(ObjectMapper objectMapper) {
 		return new ModelResolver(objectMapper);
-
 	}
-
 }
