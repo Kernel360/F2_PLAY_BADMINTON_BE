ad90c687cd3782bc6002cd2d1691c9b57ef5a743
soeunnPark <carol0724@naver.com>
2024-09-23 15:50:12 +0900

    refactor: 컨트롤러에 crud 추가

--- badminton-api/src/main/java/org/badminton/api/club/controller/ClubController.java
+++ badminton-api/src/main/java/org/badminton/api/club/controller/ClubController.java
@@ -2,11 +2,19 @@ package org.badminton.api.club.controller;
 
 import org.badminton.api.club.model.dto.ClubCreateRequest;
 import org.badminton.api.club.model.dto.ClubCreateResponse;
+import org.badminton.api.club.model.dto.ClubDeleteResponse;
+import org.badminton.api.club.model.dto.ClubReadResponse;
+import org.badminton.api.club.model.dto.ClubUpdateRequest;
+import org.badminton.api.club.model.dto.ClubUpdateResponse;
 import org.badminton.api.club.service.ClubService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.v3.oas.annotations.Operation;
@@ -20,12 +28,41 @@ public class ClubController {
 
 	private final ClubService clubService;
 
+	@GetMapping
+	@Operation(summary = "동호회 조회",
+		description = "동호회를 조회합니다.",
+		tags = {"Club"})
+	public ResponseEntity<ClubReadResponse> readClub(@RequestParam Long clubId) {
+		ClubReadResponse clubReadResponse = clubService.readClub(clubId);
+		return ResponseEntity.ok(clubReadResponse);
+	}
+
 	@PostMapping
 	@Operation(summary = "동호회 추가",
 		description = "동호회를 추가합니다.",
 		tags = {"Club"})
-	public ResponseEntity<ClubCreateResponse> createClub(@Valid @RequestBody ClubCreateRequest clubAddRequest) {
-		ClubCreateResponse clubAddResponse = clubService.createClub(clubAddRequest);
+	public ResponseEntity<ClubCreateResponse> createClub(@Valid @RequestBody ClubCreateRequest clubCreateRequest) {
+		ClubCreateResponse clubAddResponse = clubService.createClub(clubCreateRequest);
 		return ResponseEntity.ok(clubAddResponse);
 	}
+
+	@PatchMapping
+	@Operation(summary = "동호회 수정",
+		description = "동호회를 수정합니다.",
+		tags = {"Club"})
+	public ResponseEntity<ClubUpdateResponse> updateClub(@RequestParam Long clubId,
+		@Valid @RequestBody ClubUpdateRequest clubUpdateRequest) {
+		ClubUpdateResponse clubUpdateResponse = clubService.updateClub(clubUpdateRequest, clubId);
+		return ResponseEntity.ok(clubUpdateResponse);
+	}
+
+	@DeleteMapping
+	@Operation(summary = "동호회 삭제",
+		description = "동호회를 삭제합니다.",
+		tags = {"Club"})
+	public ResponseEntity<ClubDeleteResponse> deleteClub(@RequestParam Long clubId) {
+		ClubDeleteResponse clubDeleteResponse = clubService.deleteClub(clubId);
+		return ResponseEntity.ok(clubDeleteResponse);
+	}
+
 }
