db47e094dc85d52ed18c97988ed71756b56ee6ba
I-migi <sunnn10189@gmail.com>
2024-10-01 22:33:42 +0900

    feat: oAuth 로그인 로직 변경 + role 추가

--- badminton-api/src/main/java/org/badminton/api/member/oauth2/dto/CustomOAuth2Member.java
+++ badminton-api/src/main/java/org/badminton/api/member/oauth2/dto/CustomOAuth2Member.java
@@ -1,11 +1,13 @@
 package org.badminton.api.member.oauth2.dto;
 
-import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 import org.badminton.api.member.model.dto.MemberResponse;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 
 import lombok.Getter;
@@ -15,10 +17,15 @@ import lombok.RequiredArgsConstructor;
 public class CustomOAuth2Member implements OAuth2User {
 
 	private final MemberResponse memberResponse;
-	@Getter
-	private final String accessToken;
+	
 	@Getter
 	private final String registrationId;
+
+	private String clubRole;
+
+	public void updateClubRole(String clubRole) {
+		this.clubRole = clubRole;
+	}
 
 	@Override
 	public Map<String, Object> getAttributes() {
@@ -27,14 +34,12 @@ public class CustomOAuth2Member implements OAuth2User {
 
 	@Override
 	public Collection<? extends GrantedAuthority> getAuthorities() {
-		Collection<GrantedAuthority> collection = new ArrayList<>();
-		collection.add(new GrantedAuthority() {
-			@Override
-			public String getAuthority() {
-				return memberResponse.authorization();
-			}
-		});
-		return collection;
+		Set<GrantedAuthority> authorities = new HashSet<>();
+		authorities.add(new SimpleGrantedAuthority(memberResponse.authorization()));
+		if (clubRole != null && !clubRole.isEmpty()) {
+			authorities.add(new SimpleGrantedAuthority(clubRole));
+		}
+		return authorities;
 	}
 
 	@Override
@@ -44,6 +49,10 @@ public class CustomOAuth2Member implements OAuth2User {
 
 	public String getProviderId() {
 		return memberResponse.providerId();
+	}
+
+	public String getAuthorization() {
+		return memberResponse.authorization();
 	}
 
 	public Long getMemberId() {
