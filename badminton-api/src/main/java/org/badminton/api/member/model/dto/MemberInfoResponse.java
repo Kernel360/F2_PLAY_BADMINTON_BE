3b54f2fb0269b459bd2e8bca767e0c7da062be27
이선우 <sunnn10189@gmail.com>
2024-10-02 22:53:23 +0900

    feat: memberResponse 추가

---
+++ badminton-api/src/main/java/org/badminton/api/member/model/dto/MemberInfoResponse.java
@@ -0,0 +1,40 @@
+package org.badminton.api.member.model.dto;
+
+import org.badminton.domain.member.entity.MemberEntity;
+
+public record MemberInfoResponse(
+	Long memberId,
+	String name,
+	String email,
+	String profileImage
+) implements MemberDetailResponse {
+	public static MemberInfoResponse entityToMemberInfoResponse(MemberEntity memberEntity) {
+		return new MemberInfoResponse(
+			memberEntity.getMemberId(),
+			memberEntity.getName(),
+			memberEntity.getEmail(),
+			memberEntity.getProfileImage()
+		);
+	}
+
+	// Explicitly implement the methods from MemberDetailResponse
+	@Override
+	public Long getMemberId() {
+		return memberId;
+	}
+
+	@Override
+	public String getName() {
+		return name;
+	}
+
+	@Override
+	public String getEmail() {
+		return email;
+	}
+
+	@Override
+	public String getProfileImage() {
+		return profileImage;
+	}
+}
