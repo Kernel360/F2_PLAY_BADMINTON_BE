78e01f3db64aec054d97345537c08d337c3ed137
이선우 <sunnn10189@gmail.com>
2024-10-02 22:54:31 +0900

    feat: memberInfo 조회 로직 구현

--- badminton-api/src/main/java/org/badminton/api/member/controller/MemberController.java
+++ badminton-api/src/main/java/org/badminton/api/member/controller/MemberController.java
@@ -2,6 +2,7 @@ package org.badminton.api.member.controller;
 
 import org.badminton.api.member.jwt.JwtUtil;
 import org.badminton.api.member.model.dto.MemberDeleteResponse;
+import org.badminton.api.member.model.dto.MemberDetailResponse;
 import org.badminton.api.member.model.dto.MemberUpdateRequest;
 import org.badminton.api.member.model.dto.MemberUpdateResponse;
 import org.badminton.api.member.oauth2.dto.CustomOAuth2Member;
@@ -10,6 +11,7 @@ import org.badminton.domain.clubmember.repository.ClubMemberRepository;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -76,6 +78,16 @@ public class MemberController {
 		@AuthenticationPrincipal CustomOAuth2Member member) {
 		MemberDeleteResponse deleteResponse = memberService.deleteMember(member, request, response);
 		return ResponseEntity.ok(deleteResponse);
+	}
+
+	@Operation(
+		summary = "회원 정보를 조회합니다",
+		description = "회원의 마이페이지 접근 시 정보 조회 (동호회 정보 포함)",
+		tags = {"Member"}
+	)
+	@GetMapping
+	public ResponseEntity<MemberDetailResponse> getMemberInfo(@AuthenticationPrincipal CustomOAuth2Member member) {
+		return ResponseEntity.ok(memberService.getMemberInfo(member.getMemberId()));
 	}
 
 }
