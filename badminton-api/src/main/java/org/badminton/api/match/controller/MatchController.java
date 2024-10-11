1c2fd05810c79b40be13ddd9d63c01d59c3cc2b9
soeunnPark <carol0724@naver.com>
2024-10-11 18:37:04 +0900

    refactor: @RequestBody 어노테이션 추가

--- badminton-api/src/main/java/org/badminton/api/match/controller/MatchController.java
+++ badminton-api/src/main/java/org/badminton/api/match/controller/MatchController.java
@@ -12,6 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -72,7 +73,7 @@ public class MatchController {
 		@PathVariable Long leagueId,
 		@PathVariable Long matchId,
 		@PathVariable int setIndex,
-		@Valid SetScoreUpdateRequest setScoreUpdateRequest
+		@Valid @RequestBody SetScoreUpdateRequest setScoreUpdateRequest
 	) {
 		SetScoreUpdateResponse setScoreUpdateResponse = matchProgressService.updateSetScore(clubId, leagueId, matchId,
 			setIndex, setScoreUpdateRequest);
--- badminton-api/src/main/java/org/badminton/api/match/model/dto/SetScoreUpdateRequest.java
+++ badminton-api/src/main/java/org/badminton/api/match/model/dto/SetScoreUpdateRequest.java
@@ -1,6 +1,7 @@
 package org.badminton.api.match.model.dto;
 
 public record SetScoreUpdateRequest(
+	// TODO: 점수 숫자 검증해야 함. 승부가 난 숫자여야 한다.
 	int score1,
 	int score2
 ) {
