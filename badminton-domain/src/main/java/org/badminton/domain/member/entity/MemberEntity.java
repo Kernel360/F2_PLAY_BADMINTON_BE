ab95518921f86e975a65150b174e9590e4fc69d2
soeunnPark <carol0724@naver.com>
2024-10-11 15:43:15 +0900

    refactor: enum description 변경

--- badminton-domain/src/main/java/org/badminton/domain/member/entity/MemberEntity.java
+++ badminton-domain/src/main/java/org/badminton/domain/member/entity/MemberEntity.java
@@ -55,6 +55,7 @@ public class MemberEntity extends BaseTimeEntity {
 		this.profileImage = imageUrl;
 	}
 
+	// TODO: 회원 탈퇴 시 동호회에서도 탈퇴해야 함
 	public void doWithdrawal() {
 		this.isDeleted = true;
 		this.updateLastConnection();
