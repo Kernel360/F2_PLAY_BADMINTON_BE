4fafbaa5c51dd3ad59a71ce9f1876fbaa497202a
I-migi <sunnn10189@gmail.com>
2024-10-14 16:49:47 +0900

    feat: confilct resolve

--- badminton-api/src/main/java/org/badminton/api/league/service/LeagueParticipationService.java
+++ badminton-api/src/main/java/org/badminton/api/league/service/LeagueParticipationService.java
@@ -45,13 +45,12 @@ public class LeagueParticipationService {
 		LeagueEntity league = provideLeagueIfClubMemberInLeague(clubId, leagueId);
 		MemberTier requiredTier = league.getRequiredTier();
 		MemberTier clubMemberTier = clubMember.getTier();
-    checkLeagueRecruitingStatus(league);
-    checkIfClubMemberInLeague(leagueId, clubMember.getClubMemberId());
+		checkLeagueRecruitingStatus(league);
+		checkIfClubMemberInLeague(leagueId, clubMember.getClubMemberId());
 		switch (requiredTier) {
 			case GOLD -> checkGoldMemberTier(clubMemberTier, leagueId, clubId);
 			case SILVER -> checkSilverMemberTIer(clubMemberTier, leagueId, clubId);
 		}
-
 
 		LeagueParticipantEntity leagueParticipation = new LeagueParticipantEntity(clubMember, league);
 		leagueParticipantRepository.save(leagueParticipation);
@@ -70,64 +69,66 @@ public class LeagueParticipationService {
 		if (clubMemberTier != MemberTier.GOLD) {
 			throw new InsufficientTierException(MemberTier.GOLD, leagueId, clubMemberId);
 		}
-	private void checkLeagueRecruitingStatus(LeagueEntity league) {
+	}
+	private void checkLeagueRecruitingStatus (LeagueEntity league){
 		if (league.getLeagueStatus() == LeagueStatus.COMPLETED)
 			throw new LeagueRecruitingCompletedException(league.getLeagueId(), league.getLeagueStatus(),
 				league.getPlayerLimitCount());
 	}
 
-	public LeagueParticipationCancelResponse cancelLeagueParticipation(Long clubId, Long leagueId, Long memberId) {
+		public LeagueParticipationCancelResponse cancelLeagueParticipation (Long clubId, Long leagueId, Long memberId){
 
-		Long clubMemberId = provideClubMemberIfClubMemberInClub(clubId, memberId).getClubMemberId();
-		LeagueParticipantEntity leagueParticipant = provideLeagueParticipantIfClubMemberInLeague(leagueId,
-			clubMemberId);
+			Long clubMemberId = provideClubMemberIfClubMemberInClub(clubId, memberId).getClubMemberId();
+			LeagueParticipantEntity leagueParticipant = provideLeagueParticipantIfClubMemberInLeague(leagueId,
+				clubMemberId);
 
-		if (leagueParticipant.isCanceled())
-			throw new LeagueParticipationAlreadyCanceledException(leagueId, memberId);
+			if (leagueParticipant.isCanceled())
+				throw new LeagueParticipationAlreadyCanceledException(leagueId, memberId);
 
-		leagueParticipant.cancelLeagueParticipation();
-		leagueParticipantRepository.save(leagueParticipant);
+			leagueParticipant.cancelLeagueParticipation();
+			leagueParticipantRepository.save(leagueParticipant);
 
-		return LeagueParticipationCancelResponse.entityToLeagueParticipationCancelResponse(leagueParticipant);
-	}
-
-	private void checkIfClubMemberInLeague(Long leagueId, Long clubMemberId) {
-		LeagueParticipantEntity leagueParticipant = leagueParticipantRepository.findByLeague_LeagueIdAndClubMember_ClubMemberId(
-			leagueId, clubMemberId).orElse(null);
-		if (Objects.isNull(leagueParticipant)) {
-			return;
+			return LeagueParticipationCancelResponse.entityToLeagueParticipationCancelResponse(leagueParticipant);
 		}
-		if (leagueParticipant.isCanceled()) {
-			leagueParticipant.reactiveParticipation();
-		} else
-			throw new LeagueParticipationDuplicateException(leagueId, clubMemberId);
 
-	}
+		private void checkIfClubMemberInLeague (Long leagueId, Long clubMemberId){
+			LeagueParticipantEntity leagueParticipant = leagueParticipantRepository.findByLeague_LeagueIdAndClubMember_ClubMemberId(
+				leagueId, clubMemberId).orElse(null);
+			if (Objects.isNull(leagueParticipant)) {
+				return;
+			}
+			if (leagueParticipant.isCanceled()) {
+				leagueParticipant.reactiveParticipation();
+			} else
+				throw new LeagueParticipationDuplicateException(leagueId, clubMemberId);
 
-	private LeagueEntity provideLeagueIfClubMemberInLeague(Long clubId, Long leagueId) {
-		return leagueRepository.findByClubClubIdAndLeagueId(clubId, leagueId).orElseThrow(
-			() -> new LeagueNotExistException(clubId, leagueId));
-	}
-
-	private LeagueParticipantEntity provideLeagueParticipantIfClubMemberInLeague(Long leagueId, Long clubMemberId) {
-		return leagueParticipantRepository.findByLeague_LeagueIdAndClubMember_ClubMemberId(leagueId, clubMemberId)
-			.orElseThrow(
-				() -> new LeagueParticipationNotExistException(leagueId, clubMemberId));
-	}
-
-	// TODO: ban 당하거나 탈퇴한 회원의 경우 조회에서 제외
-	private ClubMemberEntity provideClubMemberIfClubMemberInClub(Long clubId, Long memberId) {
-		return clubMemberRepository.findByClub_ClubIdAndMember_MemberId(clubId, memberId).orElseThrow(
-			() -> new ClubMemberNotExistException(clubId, memberId));
-	}
-
-	private void checkPlayerCount(LeagueEntity league) {
-		// TODO: MatchCreateService와 중복 코드 발생
-		List<LeagueParticipantEntity> leagueParticipantList =
-			leagueParticipantRepository.findAllByLeague_LeagueId(league.getLeagueId());
-		if (league.getPlayerLimitCount() == leagueParticipantList.size()) {
-			league.completeLeagueRecruiting();
 		}
+
+		private LeagueEntity provideLeagueIfClubMemberInLeague (Long clubId, Long leagueId){
+			return leagueRepository.findByClubClubIdAndLeagueId(clubId, leagueId).orElseThrow(
+				() -> new LeagueNotExistException(clubId, leagueId));
+		}
+
+		private LeagueParticipantEntity provideLeagueParticipantIfClubMemberInLeague (Long leagueId, Long clubMemberId){
+			return leagueParticipantRepository.findByLeague_LeagueIdAndClubMember_ClubMemberId(leagueId, clubMemberId)
+				.orElseThrow(
+					() -> new LeagueParticipationNotExistException(leagueId, clubMemberId));
+		}
+
+		// TODO: ban 당하거나 탈퇴한 회원의 경우 조회에서 제외
+		private ClubMemberEntity provideClubMemberIfClubMemberInClub (Long clubId, Long memberId){
+			return clubMemberRepository.findByClub_ClubIdAndMember_MemberId(clubId, memberId).orElseThrow(
+				() -> new ClubMemberNotExistException(clubId, memberId));
+		}
+
+		private void checkPlayerCount (LeagueEntity league){
+			// TODO: MatchCreateService와 중복 코드 발생
+			List<LeagueParticipantEntity> leagueParticipantList =
+				leagueParticipantRepository.findAllByLeague_LeagueId(league.getLeagueId());
+			if (league.getPlayerLimitCount() == leagueParticipantList.size()) {
+				league.completeLeagueRecruiting();
+			}
+		}
+
 	}
 
-}
